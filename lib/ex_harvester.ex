f4a4f76dfd4c66d376d81a7e60c73d770e2c81ac
Joel Gregory <joelgregory7@gmail.com>
2017-11-10 15:08:53 -0500

    Printing got titles

--- lib/ex_harvester.ex
+++ lib/ex_harvester.ex
@@ -1,7 +1,13 @@
 defmodule ExHarvester do
 
   def start do
-     job = Job.last_run
+     url = Config.custodian_url <> "/2016-10-01/2016-10-04"
+     body = HttpClient.get(url)
+     titles = Poison.decode!(body, as: [%Models.Title{credits: [%Models.Title.Credit{}]}])
+     IO.puts("#{Enum.count(titles)} titles found")
+     Enum.each(titles, fn(t) ->
+       IO.puts t.title
+     end)
   end
 
 end
